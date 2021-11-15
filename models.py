dcfe7117b562959af76292f05a47320967b89776
harshakmohan <harshakmohan@gmail.com>
2021-11-15 12:47:35 -0500

    outlined models.py

--- models.py
+++ models.py
@@ -1,1 +1,23 @@
 # Define model classes here
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CNN1(torch.nn.Module):
+    def __init__(self, params): # Define all the adjustable params here
+        super(CNN1, self).__init__()
+
+
+    def forward(self, x):
+
+        return
+
+class CNN2(torch.nn.Module):
+    def __init__(self, params): # Define all the adjustable params here
+        super(CNN2, self).__init__()
+
+
+    def forward(self, x):
+
+        return
