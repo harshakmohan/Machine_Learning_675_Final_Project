4d550b86dc70a227bb864c90e1b39fb165b240ad
Gurion Marks <gurion@jhu.edu>
2021-11-25 15:17:13 -0500

    basic data load method

--- data_io.py
+++ data_io.py
@@ -1,5 +1,16 @@
-# Data Loader file
+import numpy as np
+import os
+import cv2 as cv 
 
+base = "./data"
+categories = ['cnv', 'dme', 'drusen', 'normal']
 def load():
-    return
+    data = []
+    for category in categories:
+        class_num = categories.index(category)
+        path = path.os.join(base, category)
+        for img in os.listdir(path):
+            img_arr = cv.imread(os.path.join(path, img), cv.IMREAD_GRAYSCALE)
+        data.append([img_arr, class_num])
+
 
