cdf2f63bc658939c2eb61b6996ab793f6a42cb02
Gurion Marks <gurion@jhu.edu>
2021-11-27 16:55:58 -0500

    shuffle, method name

--- data_io.py
+++ data_io.py
@@ -1,10 +1,12 @@
 import numpy as np
 import os
-import cv2 as cv 
+import cv2 as cv
+import random
 
 base = "./data"
 categories = ['cnv', 'dme', 'drusen', 'normal']
-def load():
+
+def create_dataset():
     data = []
     for category in categories:
         class_num = categories.index(category)
@@ -12,5 +14,5 @@ def load():
         for img in os.listdir(path):
             img_arr = cv.imread(os.path.join(path, img), cv.IMREAD_GRAYSCALE)
         data.append([img_arr, class_num])
-
-
+    random.shuffle(data)
+ 
