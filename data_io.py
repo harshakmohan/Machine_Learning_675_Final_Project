0626c432194e697dac067222b9131d8538189ace
Gurion Marks <gurion@jhu.edu>
2021-11-27 17:05:52 -0500

    pickle data to store

--- data_io.py
+++ data_io.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import cv2 as cv
 import random
+import pickle
 
 base = "./data"
 categories = ['cnv', 'dme', 'drusen', 'normal']
@@ -15,4 +16,23 @@ def create_dataset():
             img_arr = cv.imread(os.path.join(path, img), cv.IMREAD_GRAYSCALE)
         data.append([img_arr, class_num])
     random.shuffle(data)
+    X = []
+    y = []
+    for image, label in data:
+        X.append(image)
+        y.append(label)
+
+    X = np.array(X)
+    y = np.array(y)
+
+    out = open('X.pickle', 'wb')
+    pickle.dump(X, out)
+    out.close()
+
+    out = open('y.pickle', 'wb')
+    pickle.dump(y, out)
+    out.close()
+
+
+
  
