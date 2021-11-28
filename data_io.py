ad1e0cf6960fca50eab236d6db3c3662ee649bec
harshakmohan <harshakmohan@gmail.com>
2021-11-28 11:48:56 -0500

    debugging data_io

--- data_io.py
+++ data_io.py
@@ -11,7 +11,7 @@ def create_dataset():
     data = []
     for category in categories:
         class_num = categories.index(category)
-        path = path.os.join(base, category)
+        path = os.path.join(base, category)
         for img in os.listdir(path):
             img_arr = cv.imread(os.path.join(path, img), cv.IMREAD_GRAYSCALE)
         data.append([img_arr, class_num])
