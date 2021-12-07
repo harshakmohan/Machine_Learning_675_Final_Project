18ccef2ecc83b387c4077349271d75c16a38233f
harsha <harshakmohan@gmail.com>
2021-12-07 17:08:46 -0500

    debugging

--- models.py
+++ models.py
@@ -8,11 +8,11 @@ gc.collect()
 torch.cuda.empty_cache()
 
 class CNN1(torch.nn.Module):
-    def __init__(self, n1_chan = 80, n1_kern=5, n2_kern=10): # Define all the adjustable params here
+    def __init__(self, n1_chan = 80, n1_kern=6, n2_kern=10): # Define all the adjustable params here
         super(CNN1, self).__init__()
-        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=n1_chan, kernel_size=n1_kern, stride=1)
+        self.conv1 = torch.nn.Conv2d(in_channels=1, out_channels=n1_chan, kernel_size=n1_kern, stride=1, padding=(n1_kern//2,n1_kern//2))
         self.relu1 = nn.ReLU(inplace=True)
-        self.conv2 = torch.nn.Conv2d(in_channels=n1_chan, out_channels=4, kernel_size=n2_kern, stride=2)
+        self.conv2 = torch.nn.Conv2d(in_channels=n1_chan, out_channels=4, kernel_size=n2_kern, stride=1, padding=0)
         self.relu2 = nn.ReLU(inplace=True)
         self.flatten1 = nn.Flatten()
         self.linear1 = torch.nn.Linear(in_features=4, out_features=4)
@@ -21,16 +21,20 @@ class CNN1(torch.nn.Module):
 
     def forward(self, x):
         #print('x on cuda before? ', x.device)
+        print('size of x before reshape: ', x.size())
         x = x.reshape(x.size()[0], 1, 496, 496)
+        print('size of x after reshape: ', x.size())
         #print('x on cuda after? ', x.device)
         #self.cnn1 = nn.Sequential(self.conv1, nn.ReLU(inplace=True), self.conv2, nn.ReLU(inplace=True), self.pooling)
         #self.cnn1 = nn.Sequential(self.conv1, nn.ReLU(inplace=True), self.conv2, self.flatten1, self.linear1, self.pooling)
         x = self.conv1(x)
+        print('size of x after conv1: ', x.size())
         x = self.relu1(x)
         x = self.conv2(x)
+        print('size of x before flatten: ', x.size())
         x = self.flatten1(x)
-        print('size of x after to flatten: ', x.size(-1))
-        x = torch.nn.Linear(in_features=x.size(-1), out_features=4)(x)
+        print('size of x after to flatten: ', x.size())
+        x = torch.nn.Linear(in_features=4, out_features=4)(x)
         x = self.pooling(x)
 
 
